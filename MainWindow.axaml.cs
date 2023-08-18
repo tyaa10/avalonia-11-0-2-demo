0b05bc968e323bf5b5545361a80b25f7c7ebf7df
YuriiTrofimenko <tyaa@ukr.net>
2023-08-18 14:56:29 +0300

    added a hard task n switched publishing settings to aot

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -7,5 +7,10 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        fib(45);
+    }
+
+    private int fib(int n) { 
+        return (n == 0 || n == 1) ? 1 : fib(n - 2) + fib(n - 1);
     }
 }
